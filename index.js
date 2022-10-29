47b10983b7708360657f0d151d20c333f1e497a1
aamir <aamir.khan2478@gmail.com>
2022-10-30 01:38:57 +0500

    add validation

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const cardName= document.getElementById("cardname")
-const submit= document.getElementById("submit")
+const cardName = document.getElementById("cardname");
+const submit = document.getElementById("submit");
+const alertMessage = document.getElementById("alert-message");
+const cardNumber = document.getElementById("cardnumber");
 
-console.log("working")
+const clickHandler = (e) => {
+  e.preventDefault();
+  regNumber = /^[0-9+]{16}$/;
+  regName = /^[a-zA-Z]{0,3} [a-zA-Z]{0,3}$/;
+  if (!regName.test(cardName.value)) {
+    alertMessage.className = "alert alert-danger";
+    alertMessage.innerHTML = "Invalid card name";
+  }else if(!regNumber.test(cardNumber.value)){
+    alertMessage.className = "alert alert-danger";
+    alertMessage.innerHTML = "Invalid card number";
+  }
 
-submit.addEventListener("click",()=>{
-    console.log("working") 
-})
+  setTimeout(() => {
+    alertMessage.className = "d-none";
+  }, 2000); 
+};
+
+submit.addEventListener("click", clickHandler);
